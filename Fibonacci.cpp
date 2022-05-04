657c0077463c13bbcb8f9b5ed736176673710078
InformatycznaKuna <piotrcoolxd@gmail.com>
2022-05-04 14:07:46 +0200

    Komentarz

---
+++ Fibonacci.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>  
+using namespace std;
+int main() {
+	int n1 = 0, n2 = 1, n3, i, liczba;
+	cout << "Podaj ilość liczb: ";
+	cin >> liczba;
+	cout << n1 << " " << n2 << " ";
+	for (i = 2; i < liczba; ++i)
+	{
+		n3 = n1 + n2;
+		cout << n3 << " ";
+		n1 = n2;
+		n2 = n3;
+	}
+	return 0;
+}
